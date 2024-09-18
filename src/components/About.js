e23e4162ff02af6e70efbda471c4b811b3b01800
Breanna Camacho <breannacamacho1@gmail.com>
2024-09-18 15:56:12 -0400

    adding about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,83 @@
 import React from 'react';
+// countup
+import CountUp from 'react-countup';
+// intersection observer
+import { InView, useInView } from 'react-intersection-observer';
+// motion
+import { motion } from 'framer-motion';
+//variant
+import { fadeIn } from '../variants';
 
 const About = () => {
+  const [ref, InView] = useInView({
+    threshold: 0.5,
+  });
   return (
-  <div className='section' id='about'>
-    About
-    </div>
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div>
+          {/* image */}
+          <div className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></div>
+          {/* text */}
+          <div className='flex-1'>
+            <h2 className='h2 text-accent'>About me.</h2>
+            <h3 className='h3 mb-4'>
+              I am a passionate software developer with a background in Computer Science and Machine Learning.
+            </h3>
+            <p className='mb-6'>
+              I specialize in building interactive applications.
+            </p>
+            {/* stats */}
+            <div className='flex gap-x-6 lg:gapx10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {InView ? <CountUp start={0} end={3} duration={3} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Years of <br />
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {InView ? <CountUp start={0} end={50} duration={3} /> : null}
+                  +
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Projects <br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {InView ? <CountUp start={0} end={85} duration={3} /> : null}
+                  k+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Lines of Code <br />
+                  Writen
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {InView ? <CountUp start={0} end={26} duration={3} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Known Technologies <br />
+                  and Languages
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-x-8 items-center'>
+              <button className='btn btn-lg'>Contact me</button>
+              <a href='#' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
